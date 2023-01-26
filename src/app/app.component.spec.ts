ab2b85b38acb50b1ff6d6f0ca9ee9695ef9b7f3e
Aymar <yannyao4066@gmail.com>
2023-01-26 00:35:25 +0000

    update commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Exercice1'`, () => {
+  it(`should have as title 'Exercise1_2_3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Exercice1');
+    expect(app.title).toEqual('Exercise1_2_3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Exercice1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Exercise1_2_3 app is running!');
   });
 });
